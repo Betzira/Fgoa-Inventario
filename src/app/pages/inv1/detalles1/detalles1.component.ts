e8dc916c9bebcc851559ecf971df3a8ff0ee1dd4
Betzira <betzaira.garciav99@gmail.com>
2022-03-02 01:04:50 -0600

    Maq Editar1

--- src/app/pages/inv1/detalles1/detalles1.component.ts
+++ src/app/pages/inv1/detalles1/detalles1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalles1',
@@ -7,14 +7,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./detalles1.component.css']
 })
 export class Detalles1Component implements OnInit {
+  navigationExtras: NavigationExtras = {
+    state!: {
+    // state!: {
+      value: null
+    }
+  };
 
-  value ; //value = null;
+  articulo: any = null ; //value = null;
   constructor( private router: Router ) {
     const navigation = this.router.getCurrentNavigation();
-    this.value = navigation?.extras?.state;
+    this.articulo = navigation?.extras?.state;
    }
 
   ngOnInit(): void {
+  }
+
+  onGoToEdit(): void {
+    this.navigationExtras.state!.value = this.articulo;
+    this.router.navigate(['editar1'], this.navigationExtras);
+    // this.router.navigate(['editar1'], this.navigationExtras);
+  }
+
+  onDelete(): void {
+    alert('SE HA ELIMINADO');
+  }
+
+  onGoBackToList(): void {
+    this.router.navigate(['lista1']);
   }
 
 }
